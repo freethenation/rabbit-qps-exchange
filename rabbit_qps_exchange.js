c0d28ea8eaf013b137ebf6487e9bbce115c3eb62
Richard Klafter <richard@zinc.com>
2018-06-30 16:09:29 -0700

    inital commit

---
+++ rabbit_qps_exchange.js
@@ -0,0 +1,272 @@
+#!/usr/bin/env node
+const resolveUrl = require('url').resolve
+const request = require('request-promise-native')
+const amqplib = require('amqplib')
+const Lock = require('semaphore-async-await').Lock
+
+function showHelp(){
+    console.error(`
+Creates a rabbit exchange, 'qps_exchange', which when published to enforces a QPS across queues using parameters provided via rabbit message headers.
+Publish your messages to 'qps_exchange' instead of the default exchange supplying a 'qps-key' header and a 'qps-delay' header.
+The 'qps-key' is used to serialize all messages sent to the 'qps_exchange' regardless of their routing key which will be preserved
+The 'qps-delay' header (in milliseconds) delays the message before routeing it using the default exchange. This can be used to approximate a desired QPS.
+You may also optionally provide a 'qps-max-priority' to add a priority to the serialization and then use rabbit priority as normal
+
+Usage: ./rabbit_qps_shovel.js [OPTIONS] COMMAND
+
+commands:
+    help    show this message
+    init    create exchange and required queues then exit
+    clean   clean up idle queues then exit
+    start   ensure exchange and required queues, clean idle queues, and then start processing messages
+
+options:
+    --management Rabbit management connection string. Example http://guest:guest@localhost:15672/ (REQUIRED)
+    --connection Rabbit connection string. See https://www.rabbitmq.com/uri-spec.html (REQUIRED)
+    `.trim())
+    process.exit(1)
+}
+
+/**
+ * Returns all the queues for a rabbit server using the management http interface
+ * @param {string} managementConnString
+ * @param {string} filterRegex If supplied queues will first be filtered by name
+ */
+async function listQueues(managementConnString, filterRegex){
+    filterRegex = filterRegex || ".*"
+    if(typeof(filterRegex)=="string") filterRegex = new RegExp(filterRegex)
+    var url = resolveUrl(managementConnString, '/api/queues')
+    var queues = await request({url:url, json:true})
+    return queues.filter(queue=>filterRegex.test(queue.name))
+}
+module.exports.listQueues = listQueues
+
+/** Given a message this function parses all relevant headers  */
+function parseHeaders(msg){
+    var qpsMaxPriority = parseInt(msg.properties.headers['qps-max-priority'])
+    qpsMaxPriority = isNaN(qpsMaxPriority) ? undefined : qpsMaxPriority
+    return {
+        qpsKey: msg.properties.headers['qps-key'] || null,
+        qpsDelay: msg.properties.headers['qps-delay'] || 1000,
+        qpsMaxPriority: qpsMaxPriority,
+    }
+}
+
+/** Utility function to sleep for a given number of milliseconds */
+function sleep(milliseconds){
+    return new Promise((resolve)=>setTimeout(resolve, milliseconds))
+}
+module.exports.sleep = sleep
+
+class QpsExchange {
+    constructor({rabbitConnString, managementConnString}){
+        //settings
+        this.rabbitConnString = rabbitConnString
+        this.managementConnString = managementConnString
+        //state
+        this._consumerTags = {} //map from queue name to consumer tag promise. Used to ensure there is only ever one consumer for a queue
+        this.conn = null //rabbit connection
+        this.ch = null //rabbit channel
+    }
+    async _forwardMessage(msg, exchange){
+        await this.ch.publish(exchange||'', msg.fields.routingKey, msg.content, {
+            headers: msg.properties.headers,
+            priority: msg.priority
+        })
+    }
+    /**
+     * Connects to rabbit
+     */
+    async connect(){
+        this.conn = await amqplib.connect(this.rabbitConnString)
+        this.ch = await this.conn.createChannel()
+        this.ch.on("error", function(err) {
+            console.error("[AMQP] channel error", err.message)
+        })
+        this.ch.on("close", function() {
+            console.error("[AMQP] channel closed")
+        })
+    }
+    /**
+     * Inits exchanges and required queues
+     */
+    async initExchanges(){
+        /*
+            Exchange logic is
+            1. You publish your message to `qps_exchange` instead of the default exchange supplying a `qps-key` header and a `qps-delay` header
+                `qps-key` serializes all msgs sent to the `qps_exchange` and delays `qps-delay` (in milliseconds) before routeing then using the default exchange
+            2. If your `qps-key` has been seen before a binding in rabbit will automagically put your msg in the correct qps queue (step #4)
+            3. Else, your message ends up the QPS_UNKNOWN_QUEUE and a consumer creates a binding for future messages and forwards the msg to the correct qps queue
+            4. A consumer ensures your desired qps is enforced (by looking at the `qps-delay` header) before youzr message is sent to the default exchange
+        */
+        await this.ch.assertExchange('qps_unknown_exchange', 'topic', {durable:true})
+        await this.ch.assertQueue('qps_unknown_queue', {durable:true, maxLength:100000})
+        await this.ch.bindQueue('qps_unknown_queue', 'qps_unknown_exchange', '#') //routes all messages to the 'qps_unknown_queue'
+        await this.ch.assertExchange('qps_exchange', 'headers', {durable:true, alternateExchange:'qps_unknown_exchange'}) //The first time we see a qps-key it is routed to the QPS_UNKNOWN_EXCHANGE via alternateExchange
+    }
+    /**
+     * Start a consumer for any unknown 'qps-key'
+     */
+    async consumeQpsUnknownQueue(){
+        if(this._consumerTags['qps_unknown_queue']) return this._consumerTags['qps_unknown_queue']
+        async function consume(msg) {
+            if(msg == null){
+                //rabbit has canceled us http://www.rabbitmq.com/consumer-cancel.html
+                delete this._consumerTags['qps_unknown_queue']
+                return
+            }
+            var {qpsKey, qpsMaxPriority} = parseHeaders(msg)
+            if(!qpsKey){
+                this.ch.nack(msg, false, false) //did not set a qpsKey which is required for this exchange
+                return
+            }
+            //ensure queue and binding to handle future messages
+            await this.ch.assertQueue(`qps_key_${qpsKey}`, {maxPriority:qpsMaxPriority, durable:false, maxLength:100000}) //XXX: messages with the same qpsKey and diff maxPriority WILL bork the channel
+            await this.consumeQpsQueue(qpsKey) //start a consumer of the queue if there is not one
+            //forward the message back through the qps_exchange which should hit the binding we just created
+            await this._forwardMessage(msg, 'qps_exchange')
+            await this.ch.ack(msg)
+        }
+        consume = consume.bind(this)
+        var consumerTagPromise = this.ch.consume('qps_unknown_queue', consume).then(r=>r.consumerTag) //NOTE: We can not yield before we set this key or we will create multiple consumers
+        this._consumerTags['qps_unknown_queue'] = consumerTagPromise
+        return await consumerTagPromise //can be used to cancel the consume
+    }
+    /**
+     * Starts a consumer that shovels all message for a given `qps-key`. The consumer will respect the `qps-delay` header
+     * @param {*} qpsKey The `qps-key` to shovel messages for
+     */
+    async consumeQpsQueue(qpsKey){
+        if(this._consumerTags[`qps_key_${qpsKey}`]) return this._consumerTags[`qps_key_${qpsKey}`]
+        var lock = new Lock()
+        async function consume(msg){
+            if(msg == null){
+                //rabbit has canceled us http://www.rabbitmq.com/consumer-cancel.html
+                delete this._consumerTags[`qps_key_${qpsKey}`]
+                return
+            }
+            var {qpsDelay} = parseHeaders(msg)
+            await lock.acquire()
+            await sleep(qpsDelay)
+            lock.release()
+            //XXX: forwarding to a queue that does not exist borks the channel!
+            await this._forwardMessage(msg) //send the message to the default exchange / intended queue
+            await this.ch.ack(msg)
+        }
+        consume = consume.bind(this)
+        var consumerTagPromise = this.ch.consume(`qps_key_${qpsKey}`, consume).then(r=>r.consumerTag)
+        this._consumerTags[`qps_key_${qpsKey}`] = consumerTagPromise //NOTE: We can not yield before we set this key or we will create multiple consumers
+        await this.ch.bindQueue(`qps_key_${qpsKey}`, 'qps_exchange', '', {'qps-key':qpsKey})
+        return await consumerTagPromise //can be used to cancel the consume
+    }
+    /**
+     * Cancels all consumers in preparation for shutdown
+     */
+    async cancelAllConsumers(){
+        await Promise.all(Object.keys(this._consumerTags)
+            .map(this.cancelConsumer.bind(this)))
+    }
+    /**
+     * Cancels the consumer for a given queue iff there is one
+     * @param {*} queue The queue to stop consuming
+     */
+    async cancelConsumer(queue){
+        if(!this._consumerTags[queue]) return
+        await this.ch.cancel(await this._consumerTags[queue])
+        delete this._consumerTags[queue]
+    }
+    /**
+     * Uses the management interface to discover and existing `qps-key` queues and starts a consumer for them
+     */
+    async consumeExistingQpsQueues(){
+        var queues = await listQueues(this.managementConnString, '^qps_key_')
+        await Promise.all(queues.map(q=>q.name).map(this.consumeQpsQueue.bind(this)))
+    }
+    /**
+     * Delete any queues that have not been used recently
+     * @param {*} maxIdleDuration If a queue has been for this duration (in milliseconds) it will be deleted. The default is 1hr
+     */
+    async deleteIdleQueues(maxIdleDuration){
+        maxIdleDuration = maxIdleDuration || 60*60*1000 //1hr
+        var now = +(new Date()) + (new Date()).getTimezoneOffset()*60*1000 //if server is not in UTC we need to correct
+        var idleQueues = (await listQueues(this.managementConnString, '^qps_key_'))
+            .filter(q=>{
+                var idleSince = q.idle_since ? +(new Date(q.idle_since)) : now //if the queue is very new it wont have 'idle_since'
+                return now - idleSince  > maxIdleDuration
+            })
+            .map(q=>q.name)
+        await Promise.all(idleQueues.map(q=>this.cancelConsumer(q)))
+        await Promise.all(idleQueues.map(q=>this.ch.deleteQueue(q)))
+    }
+}
+module.exports.QpsExchange = QpsExchange
+
+async function shutdown(exchange, cleanIdleQueuesIntervalId){
+    var log = console.error
+    log('detected SIGINT shutting down cleanly...')
+    log('stopping cleanup of idle queues...')
+    clearInterval(cleanIdleQueuesIntervalId)
+    log('canceling consumers...')
+    await exchange.cancelAllConsumers()
+    log('closing connection...')
+    await exchange.conn.close()
+    //process.exit(0) //should exit on it's own cause node's event loop will be empty
+}
+
+async function main(argv){
+    //parse / validate cmd line
+    var log = console.error
+    var command = ((argv.argv||[])[0] || 'help').toLowerCase()
+    if(argv.h || argv.help || command=="help") showHelp()
+    if(!argv.connection){ log('Error: --connection is required\n'); showHelp() }
+    if(!argv.management){ log('Error: --management is required\n'); showHelp() }
+    if(['init','start','clean'].indexOf(command)==-1){ showHelp() }
+
+    //connect and stuff
+    var exchange = new QpsExchange({
+        rabbitConnString: argv.connection,
+        managementConnString: argv.management,
+    })
+    log('connecting to rabbit...')
+    await exchange.connect()
+    function handleRabbitError(){
+        log('rabbit error: '+JSON.stringify(Array.from(arguments)))
+        process.exit(1)
+    }
+    exchange.conn.on('close', handleRabbitError.bind(null, 'connection close'))
+    exchange.conn.on('error', handleRabbitError.bind(null, 'connection error'))
+    exchange.ch.on('close', handleRabbitError.bind(null, 'channel close'))
+    exchange.ch.on('error', handleRabbitError.bind(null, 'channel error'))
+
+    //run the actual command
+    if(command == "init" || command == "start"){
+        log('initializing required exchanges and queues...')
+        await exchange.initExchanges()
+    }
+    if(command == "clean" || command == "start"){
+        log("cleaning up idle queues...")
+        await exchange.deleteIdleQueues()
+    }
+    if(command == "start"){
+        log("consuming existing queues...")
+        await exchange.consumeExistingQpsQueues()
+        log("consuming new/unknown 'qps-key'...")
+        await exchange.consumeQpsUnknownQueue()
+        log('scheduling automatic cleanup of idle queues...')
+        var cleanIdleQueuesIntervalId = setInterval(exchange.deleteIdleQueues.bind(exchange), 10*60*1000) //every 10min
+        log('registering for SIGINT handling...')
+        process.on('SIGINT', shutdown.bind(this, exchange, cleanIdleQueuesIntervalId))
+    }
+    log('success!')
+}
+module.exports.main = main //only exported for testing
+
+if (require.main === module) {
+    main(require('argh').argv)
+    /* you can queue a test message with the following commands
+    $ ./rabbit_qps_exchange.js --connection "amqp://localhost" --management "http://guest:guest@localhost:15672/" start &
+    $ rabbitmqadmin declare queue name=test durable=false
+    $ rabbitmqadmin publish exchange=qps_exchange routing_key=test payload="$(date)" properties='{"headers":{"qps-key":"test"}}'
+    $ rabbitmqadmin get queue=test requeue=false
+    */
+}
---
+++ test/test.js
@@ -0,0 +1,233 @@
+var assert = require('assert')
+var {QpsExchange, sleep, listQueues, main} = require('../rabbit_qps_exchange')
+
+var rabbitConnString = process.env.rabbitConnString || "amqp://localhost"
+var managementConnString = process.env.managementConnString || "http://guest:guest@localhost:15672/"
+
+describe('QpsExchange', function() {
+    this.timeout(5000);
+    var exchange = null
+
+    //init state and cleanup before and after each test
+    beforeEach(async function(){
+        console.log('starting new test...')
+        exchange = new QpsExchange({managementConnString, rabbitConnString})
+        await exchange.connect()
+    })
+    afterEach(async function(){
+        //close test's connection
+        try{ await exchange.conn.close() } catch(err){}
+        exchange = null
+        //cleanup stuff that might have been created
+        console.log('creating connection for cleanup and cleaning...')
+        var e = new QpsExchange({managementConnString, rabbitConnString})
+        await e.connect()
+        await Promise.all([
+            e.ch.deleteQueue('qps_key_test1'),
+            e.ch.deleteQueue('qps_key_test2'),
+            e.ch.deleteQueue('destination_test1'),
+            e.ch.deleteQueue('destination_test2'),
+            e.ch.deleteQueue('qps_unknown_queue'),
+            e.ch.deleteExchange('qps_unknown_exchange'),
+            e.ch.deleteExchange('qps_exchange'),
+        ])
+        await e.conn.close()
+    })
+
+    it('should connect', async function() {
+        //await exchange.connect() //done in beforeEach
+    })
+
+    it('should init exchanges', async function(){
+        await exchange.initExchanges()
+        await exchange.ch.checkQueue('qps_unknown_queue')
+        await exchange.ch.checkExchange('qps_unknown_exchange')
+        await exchange.ch.checkExchange('qps_exchange')
+    })
+
+    it('forwardMessage should work', async function(){
+        var ch = exchange.ch
+        var hasForwarded = false
+        await ch.assertQueue(`qps_key_test1`, {durable:false})
+        await ch.sendToQueue(`qps_key_test1`, new Buffer('content'), {headers:{'qps-key':'test1'}})
+        await new Promise(async function(resolve, reject){
+            await ch.consume('qps_key_test1', async function(msg) {
+                //implicitly assert routeing key did not change (which has to be the case if this function is called a 2nd time)
+                assert.equal(msg.properties.headers['qps-key'], 'test1') //assert headers are preserved
+                assert.equal(msg.content.toString(), 'content') //assert content is preserved
+                ch.ack(msg)
+                if(!hasForwarded){
+                    hasForwarded = true //only forward once
+                    exchange._forwardMessage(msg, '') //forward msg to default exchange... aka back to this queue
+                } else {
+                    resolve()
+                }
+            })
+        })
+    })
+
+    it('should end up in the correct queue', async function(){
+        var ch = exchange.ch
+        await ch.assertQueue(`qps_key_test1`, {durable:false})
+        await ch.assertQueue('destination_test1', {durable:false})
+        //init the exchange
+        await exchange.initExchanges()
+        await exchange.consumeQpsQueue('test1') //create binding for qps_key_test1 created above
+        var publishTime = +(new Date())
+        await ch.publish('qps_exchange', 'destination_test1', new Buffer('content'), {
+            headers:{'qps-key':'test1','qps-delay':'1000'}
+        })
+        //check to see if message ended up in the correct place
+        var msg = await receiveSingleMessage(ch, 'destination_test1')
+        var receiveTime = msg.time
+        console.info(`it took ${receiveTime - publishTime}ms for the message to arrive`)
+        assert.ok(receiveTime - publishTime > 1000, 'message arrived too quickly')
+        assert.ok(receiveTime - publishTime < 1500, 'message arrived too slowly')
+        assert.equal(msg.properties.headers['qps-key'], 'test1') //assert headers are preserved
+        assert.equal(msg.content.toString(), 'content') //assert content is preserved
+    })
+
+    it('should create queues for unknown "qps-key"', async function(){
+        var ch = exchange.ch
+        await ch.assertQueue('destination_test1', {durable:false})
+        await exchange.initExchanges()
+        await exchange.consumeQpsUnknownQueue()
+        await ch.publish('qps_exchange', 'destination_test1', new Buffer('content2'), {
+            headers:{'qps-key':'test1','qps-delay':'100'}
+        })
+        await sleep(500) //so there is enough time to create unknown queue
+        await ch.checkQueue('qps_key_test1')
+        //check to see if message ended up in the correct place
+        var msg = await receiveSingleMessage(ch, 'destination_test1')
+        assert.equal(msg.properties.headers['qps-key'], 'test1') //assert headers are preserved
+        assert.equal(msg.content.toString(), 'content2') //assert content is preserved
+    })
+
+    //if you use this method you should only expect to receive X messages ever from a queue even if you consume elsewhere... it is a hack for testing that cleans up poorly
+    //it also adds a time key to the message which is the time the message was received
+    function receiveXMessages(ch, queue, numberOfMessages, timeout){
+        timeout = timeout || 2000
+        var messages = []
+        return new Promise(async function(resolve, reject){
+            setTimeout(reject.bind(null, 'message arrived too slow or not at all'), timeout)
+            await ch.consume(queue, function(msg){
+                msg.time=+(new Date())
+                ch.ack(msg)
+                messages.push(msg)
+                if(messages.length >= numberOfMessages) resolve(messages)
+            })
+        })
+    }
+    async function receiveSingleMessage(ch, queue){
+        return (await receiveXMessages(ch, queue, 1, 2000))[0]
+    }
+
+    it('should enforce qps across queues', async function(){
+        var ch = exchange.ch
+        await ch.assertQueue('destination_test1', {durable:false})
+        await ch.assertQueue('destination_test2', {durable:false})
+        await exchange.initExchanges()
+        await exchange.consumeQpsUnknownQueue()
+        //publish
+        var publishTime = +(new Date())
+        await ch.publish('qps_exchange', 'destination_test1', new Buffer('content1'), {
+            headers:{'qps-key':'test1','qps-delay':'500'}
+        })
+        await sleep(100) //sleep to ensure message order cause awaiting a publish does not confirm without rabbit extension/mode
+        await ch.publish('qps_exchange', 'destination_test2', new Buffer('content2'), {
+            headers:{'qps-key':'test1','qps-delay':'500'}
+        })
+        //check results
+        var [msg1, msg2] = await Promise.all([
+            receiveSingleMessage(ch, 'destination_test1'),
+            receiveSingleMessage(ch, 'destination_test2'),
+        ])
+
+        //this.timeout(60*60*1000)
+        //await sleep(60*60*1000)
+
+        assert.equal(msg1.properties.headers['qps-key'], 'test1') //assert headers are preserved
+        assert.equal(msg1.content.toString(), 'content1') //assert content is preserved
+        assert.equal(msg2.properties.headers['qps-key'], 'test1') //assert headers are preserved
+        assert.equal(msg2.content.toString(), 'content2') //assert content is preserved
+
+        console.info(`it took ${msg1.time - publishTime}ms for msg1 to arrive`)
+        console.info(`it took ${msg2.time - publishTime}ms for msg2 to arrive`)
+        assert.ok(msg1.time - publishTime > 500, 'message 1 arrived too quickly')
+        assert.ok(msg1.time - publishTime < 750, 'message 1 arrived too slowly')
+        assert.ok(msg2.time - publishTime > 1000, 'message 2 arrived too quickly')
+        assert.ok(msg2.time - publishTime < 1250, 'message 2 arrived too slowly')
+    })
+
+
+    it('should respect message priority', async function(){
+        var ch = exchange.ch
+        await ch.assertQueue('destination_test1', {durable:false})
+        await exchange.initExchanges()
+        await exchange.consumeQpsUnknownQueue()
+
+        //publish a message to get things created
+        var publishTime = +(new Date())
+        await ch.publish('qps_exchange', 'destination_test1', new Buffer('content0'), {
+            priority:1, headers:{'qps-key':'test1','qps-delay':'10', 'qps-max-priority':'2'}
+        })
+        await sleep(250) //wait for message to be delivered
+
+        //stop consuming from qps_key_test1 so we can test priority
+        await exchange.cancelConsumer('qps_key_test1')
+        //queue some message to test priority
+        await ch.publish('qps_exchange', 'destination_test1', new Buffer('content2'), {
+            priority:1, headers:{'qps-key':'test1','qps-delay':'10', 'qps-max-priority':'2'}
+        })
+        await sleep(250) //sleep to ensure message order cause awaiting a publish does not confirm without rabbit extension/mode
+        await ch.publish('qps_exchange', 'destination_test1', new Buffer('content1'), {
+            priority:2, headers:{'qps-key':'test1','qps-delay':'10', 'qps-max-priority':'2'}
+        })
+        await sleep(250) //wait for messages to be published
+        //restart consumer
+        await exchange.consumeQpsQueue('test1')
+
+        //read the messages and assert stuff
+        var [_, msg1, msg2] = await receiveXMessages(ch, 'destination_test1', 3)
+        assert.equal(msg1.properties.headers['qps-key'], 'test1') //assert headers are preserved
+        assert.equal(msg1.content.toString(), 'content1') //assert content is preserved
+        assert.equal(msg2.properties.headers['qps-key'], 'test1') //assert headers are preserved
+        assert.equal(msg2.content.toString(), 'content2') //assert content is preserved
+    })
+
+    it('command line should run', async function(){
+        await main({
+            argv:['init'],
+            connection:rabbitConnString,
+            management:managementConnString,
+        })
+        await main({
+            argv:['clean'],
+            connection:rabbitConnString,
+            management:managementConnString,
+        })
+        await main({
+            argv:['start'],
+            connection:rabbitConnString,
+            management:managementConnString,
+        })
+    })
+
+    //works but slow :(
+    it('should del idle queues', async function(){
+        //init testing queues
+        await exchange.ch.assertQueue(`qps_key_test1`, {durable:false})
+        await sleep(10*1000) //age the queue... the rabbit management API has a lot of lag so we age for a long time
+        await exchange.ch.assertQueue(`qps_key_test2`, {durable:false})
+        //do actual del of idle queues
+        await exchange.deleteIdleQueues(5000)
+        //check the results
+        await exchange.ch.checkQueue('qps_key_test2') //queue should still be there and this should not throw
+        try{
+            await exchange.ch.checkQueue('qps_key_test1')
+        } catch(err){
+            return
+        }
+        assert.ok(false, "idle queue should have been deleted")
+    }).timeout(15000)
+})
